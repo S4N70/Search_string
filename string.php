b72b28a4f3e0fa8c4d6a26afaca229daabb2f522
S4N7010 <108127986+S4N7010@users.noreply.github.com>
2022-10-09 20:42:19 +0200

    Let's go

---
+++ string.php
@@ -0,0 +1,11 @@
+<?php 
+    
+    $string = $_GET["string"];
+    if($string==""){
+    echo "la stringa inserita e' vuota :( " ;
+    }
+    else{
+        echo "la stringa inserita e' " . $string ;
+    }
+    $pattern = "";
+?>
