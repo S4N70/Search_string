d37dbcf9d653e6d604e292cf7a75068616fa3075
S4N7010 <108127986+S4N7010@users.noreply.github.com>
2022-10-15 19:26:15 +0200

    php structure defined

--- string.php
+++ string.php
@@ -2,10 +2,16 @@
     
     $string = $_GET["string"];
     if($string==""){
-    echo "la stringa inserita e' vuota :( " ;
+    echo "La stringa inserita e' vuota :( " ;
     }
     else{
-        echo "la stringa inserita e' " . $string ;
+        echo "La stringa inserita e' " . $string ;
     }
-    $pattern = "";
+    $length = strlen($string);
+    $string= str_replace(" ", "", $string); /// per effettuare una ricerca senza conteggiare gli spazi ///
+    $last5 = $length - 5;
+    echo "<br>" . "La stringa inserita contiene " . strlen($string) . " caratteri.";
+    echo "<br>" . "I primi 5 caratteri della stringa sono: " . substr($string, 0, 5);
+    echo "<br>" . "Gli ultimi 5 caratteri della stringa sono: " . substr($string, $last5, 5);
+
 ?>
