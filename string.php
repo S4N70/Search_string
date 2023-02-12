fdbf83b900e562dace6164c6ff2a0348c6dbd428
S4N70 <108127986+S4N70@users.noreply.github.com>
2023-02-12 01:11:32 +0100

    Finish

--- string.php
+++ string.php
@@ -5,71 +5,92 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="styles.css" rel="stylesheet" type="text/css">
-    <title> String  </title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;600&display=swap" rel="stylesheet">
+    <title> SearchString </title>
 </head>
-<body>
-    <div id="box1" class="margin">
-        <div id="stringtitle"><?php
-        use function PHPSTORM_META\type;
-        $string = $_GET["string"];
-        if($string==""){
-            echo "La stringa inserita e' vuota :( " ;
-        }
-        else{
-            echo "La stringa inserita e':   " . $string ;
-        }
-        ?>
-        </div>
-        <div class="container">
+<body>   
+<div class="flex-container">
+    <h1 class="center text-result"> 
         <?php
-        $length = strlen($string);
-        $string= str_replace(" ", "", $string); /// per effettuare una ricerca senza conteggiare gli spazi ///
-        $last5 = $length - 5;
+            use function PHPSTORM_META\type;
+            $string = $_GET["string"];
+            if($string==""){
+                echo "La stringa inserita e' vuota :( " ;
+            }
+            else{
+                echo "La stringa inserita e':   " . $string ;
+            }
+        ?>
+</div>
+<div class="flex-container">
+        <h1 class="center text-result">
+            <?php
+                $length = strlen($string);
+                $string= str_replace(" ", "", $string); /// per effettuare una ricerca senza conteggiare gli spazi ///
+                $last5 = $length - 5;
 
-        echo "<br>" . "La stringa inserita contiene " . strlen($string) . " caratteri.";
-        ?>
-        </div>
-         <div class="container">
-        <?php
-        echo "<br>" . "I primi 5 caratteri della stringa sono: " . substr($string, 0, 5);
-        echo "<br>" . "Gli ultimi 5 caratteri della stringa sono: " . substr($string, $last5, 5) . "<br>";
-        ?>
-        </div>
-        <div class="container">
+                echo "<br>" . "La stringa inserita contiene "  . "<br>" . strlen($string) . " caratteri.";
+            ?>
+        </h1>
+</div>
+<div class="flex-container">
+        <h1 class="center text-result">
+            <?php
+                echo "<br>" . "I primi 5 caratteri della stringa sono: "  . "<br>" . substr($string, 0, 5);
+            ?>
+        </h1>
+        <h1 class="center text-result">
+            <?php
+                echo "<br>" . "Gli ultimi 5 caratteri della stringa sono: " . "<br>"  . substr($string, $last5, 5);
+            ?>
+        </h1>
+</div>
+<div class="flex-container">
+    <h1 class="center text-result">
         <?php
         $num = [];
         $numeri = 0;
         $revnum= [];
-
-        echo "i primi 5 numeri all'interno della stringa sono: ";
+        echo "i primi 5 numeri all'interno della stringa sono: "  . "<br>";
         for($i=0; $i<$length; $i++){
-                $num[$i] = substr($string, $i, 1);
-                    if (is_numeric($num[$i])){
-                        $numeri++;
-                    if($numeri<6){
-                            echo $num[$i];
-                        }
+            $num[$i] = substr($string, $i, 1);
+            if (is_numeric($num[$i])){
+                $numeri++;
+                if($numeri<6){
+                    echo $num[$i];
+                    }
                 }
             }
-        $numeri=0;
-        echo "<br>" . "Gli ultimi 5 numeri all'interno della stringa sono: ";
-        for($i=0; $i<$length; $i++){
+            if($numeri == 0){
+                echo "non sono presenti numeri all'interno della stringa". "<br>";
+            }
+        ?>
+    </h1>
+    <h1 class="center text-result"> 
+        <?php
+            $num = [];
+            $numeri = 0;
+            $revnum= [];
+            echo "Gli ultimi 5 numeri all'interno della stringa sono: "  . "<br>";
+            for($i=0; $i<$length; $i++){
                 $num[$i] = substr(strrev($string), $i, 1);
-                    if (is_numeric($num[$i])){
-                        $revnum[$numeri]=$num[$i];
-                        $numeri++;
+                if (is_numeric($num[$i])){
+                    $revnum[$numeri]=$num[$i];
+                    $numeri++;
                     if($numeri==5){
                         for($x=4; $x>=0; $x--){
-                        echo $revnum[$x];
+                            echo $revnum[$x];
                         }
+                    }
                 }
             }
-        }
-            if($numeri==0){
-                echo "Non sono presenti numeri all'interno della stringa";
+            if($numeri == 0){
+                echo "non sono presenti numeri all'interno della stringa" . "<br>";
             }
-    ?>
-        </div>
-    </div>
+        ?>
+    </h1>
+</div>
 </body>
 </html>
